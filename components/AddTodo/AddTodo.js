826943402e4b2e6833421bf0790ae8e44cfb5faa
ManousakisGS <84785467+ManousakisGS@users.noreply.github.com>
2021-05-27 16:40:09 +0300

    update Addtodo

--- components/AddTodo/AddTodo.js
+++ components/AddTodo/AddTodo.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, {Component, useState} from 'react';
 import {useDispatch} from 'react-redux';
-import {addTodo, addTodos} from '../../store/todoSlice';
+import { addTodos} from '../../store/todoSlice';
 import {nanoid} from '@reduxjs/toolkit';
 
 const AddTodo = () => {
@@ -8,25 +8,29 @@ const AddTodo = () => {
     const [text, setText] = useState('');
 
     const submit = () => {
+        
         if (text.length>0) {
-            /* dispatch(addTodo({id: nanoid(), todo: text, completed: false})); */
         const items = text.split(',');
         dispatch(
             addTodos(
                 items.map((item) => ({ id: nanoid(), text: item, completed: false }))
             )
         );        
-        }
+        };
+        document.getElementById('clear-text').value="";
+        setText('');
     };
 
     return (
         <div className='add-todo'>
+            <div >
             <p> To add multiple activities write them comma separated</p>
-            <p>
+            <p id='text-explain'>
                 <i>eg: reading, eating, coding, hiking</i>
             </p>
-            <input value={text} onChange={(e) => setText(e.target.value)} />
-            <button onClick={submit}>Add Todo</button>
+            </div>
+            <input id="clear-text" placeholder="Type here:" onfocus="this.placeholder = ''" value={text} onChange={(e) => setText(e.target.value)} />
+            <button onClick={submit}>Add activity</button>
         </div>
     );
 };
